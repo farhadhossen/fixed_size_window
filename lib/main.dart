001867d618431ee920fcebffb3429f7bf3daac62
farhadhossen <imfarhad01@gmail.com>
2022-02-24 16:24:34 +0600

    fixed size app window done

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 void main() {
   runApp(
@@ -11,8 +12,15 @@ void main() {
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
       debugShowCheckedModeBanner: false,
-
     ),
-
   );
+
+  doWhenWindowReady((){
+    var initialSize = Size(600, 450);
+    appWindow.size = initialSize;
+    appWindow.minSize = initialSize;
+    appWindow.maxSize = initialSize;
+    appWindow.title = "Custom Title";
+    appWindow.show();
+  });
 }
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,5 +1,5 @@
 #include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
-auto bdw = bitsdojo_window_configure(BDW_CUSTOM_FRAME );
+auto bdw = bitsdojo_window_configure(BDW_CUSTOM_FRAME | BDW_HIDE_ON_STARTUP);
 //| BDW_HIDE_ON_STARTUP
 //custom frame for hide app bar and hide on means when app start it will be hidden
 //thats why a blink not show to as
